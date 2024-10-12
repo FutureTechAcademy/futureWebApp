ddb8fcc80139f7af27de2175def6dd4bc25da326
SWEETSIVA <rsuvisivadme2000@gmail.com>
2024-10-12 20:57:21 +0530

    hello1

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,31 +5,4 @@ let year = date.getFullYear()
 let copyright="@ Future Tech Academy "+year+", All rights reserved"
 copyPara.innerHTML=copyright
 
-// SlideShow
-let currentIndex = 0;
-
-function showSlide(index) {
-  const slides = document.querySelectorAll('.slider div');
-  const totalSlides = slides.length;
-
-  // Reset all slides
-  slides.forEach(slide => {
-    slide.style.display = 'none';
-  });
-
-  // Show current slide
-  slides[index].style.display = 'block';
-}
-
-// Next Slide Function
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % document.querySelectorAll('.slider div').length;
-  showSlide(currentIndex);
-}
-
-// Initialize first slide
-showSlide(currentIndex);
-
-// Change slide every 3 seconds
-setInterval(nextSlide, 3000);
 
